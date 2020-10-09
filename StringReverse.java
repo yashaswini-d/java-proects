7e2a84979dda48a15520bf101126b9de523ae9dd
yashaswini-d <61838341+yashaswini-d@users.noreply.github.com>
2020-10-09 13:55:23 +0530

    Add files via upload

---
+++ StringReverse.java
@@ -0,0 +1,17 @@
+package string_example;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		
+		String input = " hi this is yashaswini ";
+		StringBuilder input1 = new StringBuilder();
+		 input1.append(input);
+		 input1 = input1.reverse();
+		 
+		 System.out.println(input1);
+		 
+
+	}
+
+}
